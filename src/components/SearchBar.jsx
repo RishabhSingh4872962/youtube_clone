7bb40213276d57cbae460e1a2ac42bb6b2bbe26f
Rishabh <rs4872962@gmail.com>
2023-01-10 07:44:43 +0530

    Second commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,22 +6,27 @@ import { useNavigate } from "react-router-dom";
 const SearchBar = () => {
   return (
     <Paper
-    component='form'
-    onSubmit={()=>{}}
-    sx={{ borderRadius:20 ,pl:2,border: "1px solid #e3e3e3",boxShadow:"none",mr:{sm:5}}}
-
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        pl: 2,
+        border: "1px solid #e3e3e3",
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
     >
-    <input 
-    className="search-bar"
-    placeholder="search..."
-   // value=''
-    //onChange={()=>{}}
-    />
-    <IconButton type="submit" sx={{ p:"10px",color:"red"}}>
-   <Search/>
-    </IconButton>
-    
-    </Paper>);
+      <input
+        className="search-bar"
+        placeholder="search..."
+        // value=''
+        //onChange={()=>{}}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
